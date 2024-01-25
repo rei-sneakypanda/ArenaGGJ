662cdd0b79e2cf75943de60e3ed3b44cd2abb348
Hatuls <64154408+Hatuls@users.noreply.github.com>
2024-01-25 22:23:18 +0200

    stat handler

---
+++ Assets/Scripts/StatsHandler.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Collections.Generic;
+    public class Stat : IStat
+    {
+        public event Action OnReset;
+        public event Action<float> OnValueChanged;
+
+        public StatType StatType { get;private set; }
+        private float _startingValue;
+        private float _value;
+        public Stat(StatType stat, float val)
+        {
+        StatType = stat;
+            _startingValue = val;
+            Value = StartValue;
+        }
+        public float StartValue => _startingValue;
+
+        public float Value
+        {
+            get => _value;
+            set
+            {
+                if (_value == value)
+                    return;
+
+                _value = value;
+                OnValueChanged?.Invoke(Value);
+            }
+        }
+
+        public void Reset()
+        {
+            OnReset?.Invoke();
+            _value = _startingValue;
+        }
+    }
+
+
+    public class StatHandler : IStatHandler
+    {
+        private readonly Dictionary<StatType, IStat> _statDictionary;
+        public StatHandler(params Stat[] stat)
+        {
+            //int statCount = statSO.Length;
+            //_statDictionary = new Dictionary<StatType, IStat>(statCount);
+            //for (int i = 0; i < statCount; i++)
+            //{
+            //    StatSO stat = statSO[i];
+            //    _statDictionary.Add(stat, new Stat(stat));
+            //}
+        }
+
+        public IStat this[StatType stat]
+        {
+            get
+            {
+                if (_statDictionary.TryGetValue(stat, out IStat istat))
+                    return istat;
+                throw new Exception($"Stat Handler: stat was not found!\nName: {stat}");
+            }
+        }
+    }
+
+    public enum StatType
+    {
+        MovementSpeed = 0,
+       Score = 1,
+       HP = 2,
+       RotationSpeed = 3,
+        Mass = 4,
+        Scale = 5,
+        SpawnTime
+
+    }
+    public interface IStat
+    {
+        event Action OnReset;
+        event Action<float> OnValueChanged;
+
+        StatType StatType { get; }
+        float StartValue { get; }
+        float Value { get; set; }
+        void Reset();
+    }
+    public interface IStatHandler
+    {
+        IStat this[StatType stat] { get; }
+    }
