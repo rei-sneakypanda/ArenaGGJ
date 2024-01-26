3c1e7de3cacba3397f30c4d6c39c955ed349d386
Rei <139059768+rei-sneakypanda@users.noreply.github.com>
2024-01-27 01:28:57 +0200

    timer

--- Assets/GameTime.cs
+++ Assets/GameTime.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using Cysharp.Threading.Tasks;
 using Sirenix.OdinInspector;
+using TMPro;
 using UniRx;
 using Unity.Collections;
 using UnityEngine;
@@ -10,6 +12,7 @@ using Random = UnityEngine.Random;
 
 public class GameTime : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI _timerText;
     public static event Action GameStarted;
     public static event Action GameEnded;
     public static GameTime Instance { get; private set; }
@@ -58,7 +61,7 @@ public class GameTime : MonoBehaviour
                     ground.AddForce((ground.position - Vector3.zero) * _explodingForce, ForceMode.Impulse);
                 }
             }
-            
+            _timerText.SetText(TimeSpan.FromSeconds((int)_remainingTime.Value).GetTimeSpanString(useTwoDigit: true));
             await UniTask.DelayFrame(1, cancellationToken: destroyCancellationToken);
         }
 
@@ -69,5 +72,58 @@ public class GameTime : MonoBehaviour
     private void OnDestroy()
     {
         Instance = null;
+    }
+}
+
+public static class TimeSpanExtensions
+{
+    private static readonly List<(Func<TimeSpan, int> Func, string Letter)> TimespanToTemplate = new()
+    {
+        (time => time.Seconds, "s"),
+        (time => time.Minutes, "m"),
+        (time => time.Hours, "h"),
+        (time => time.Days, "d")
+    };
+
+    public static string GetTimeSpanString(
+        this TimeSpan timespan,
+        int minimumVisualItemCount = 2,
+        int maxVisualItemCount = 3,
+        bool useLetter = true,
+        bool useTwoDigit = false,
+        bool hideZeroValues = false)
+    {
+        var sign = "";
+        if (timespan < TimeSpan.Zero)
+        {
+            timespan = timespan.Duration();
+            sign = "-";
+        }
+            
+        var answer = new StringBuilder(sign);
+        int addedAmount = 0;
+
+        for (int i = TimespanToTemplate.Count - 1; i >= 0 && addedAmount < maxVisualItemCount; i--)
+        {
+            bool showItem = i < minimumVisualItemCount || addedAmount > 0;
+
+            int timeAmount = TimespanToTemplate[i].Func(timespan);
+            var letter = TimespanToTemplate[i].Letter;
+
+            if (showItem || timeAmount > 0)
+            {
+                addedAmount++;
+                var isLast = i == 0 || addedAmount == maxVisualItemCount;
+                var suffix = useLetter ? letter : (!isLast ? ":" : "");
+                if (hideZeroValues && timeAmount == 0)
+                {
+                    continue;
+                }
+                    
+                answer.Append(string.Format((useTwoDigit ? "{0:D2}" : "{0}") + suffix, timeAmount));
+            }
+        }
+
+        return answer.ToString();
     }
 }
